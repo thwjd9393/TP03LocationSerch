ebce4fd1807711b7e386e28bdbe848d6509369aa
thwjd9393 <thwjd9393@naver.com>
2023-12-07 16:34:47 +0900

    create) itemVeiw 제작

--- app/src/main/java/com/airpass/tp03locationserch/SearchRecyclerAdapter.kt
+++ app/src/main/java/com/airpass/tp03locationserch/SearchRecyclerAdapter.kt
@@ -1,20 +1,29 @@
 package com.airpass.tp03locationserch
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.airpass.tp03locationserch.databinding.ViewholderSearchResultItemBinding
 
-class SearchRecyclerAdapter:RecyclerView.Adapter<> {
+//class SearchRecyclerAdapter: RecyclerView.Adapter<SearchRecyclerAdapter.SearchItemViewHolder>() {
+class SearchRecyclerAdapter {
 
-    class SearchItemViewHolder(itemView:View, val searchResultClickListener: (Any)->Unit) : RecyclerView.ViewHolder(itemView) {
-        fun binding(data:Any) = with(itemView) {
-
-        }
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-
+//    inner class SearchItemViewHolder(itemView:View, val searchResultClickListener: (Any)->Unit) : RecyclerView.ViewHolder(itemView) {
+//        fun binding(data:Any) = with(itemView) {
+//
+//        }
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchItemViewHolder {
+//        return SearchItemViewHolder
+//    }
+//
+//    override fun onBindViewHolder(holder: SearchItemViewHolder, position: Int) {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun getItemCount(): Int {
+//        TODO("Not yet implemented")
+//    }
 
 }
